b1b3ac74a1345fa8afd163f4a8f253ffb9683e59
khairulmunir <khairulmunir@gmi.edu.my>
2024-06-10 16:54:48 +0800

    Print_lesson

--- Lab1.py
+++ Lab1.py
@@ -1,1 +1,40 @@
-print('Selamat Datang ke GMI!')
+#multiline comment and uncomment - CTRL + /
+
+print("Hello")
+print("Bonjour\n") #\n -> for enter
+print("There")
+print("Hello", end="--")
+print("\"Apa khabar\"") #\ is escape character so that " can be printed
+print("Hello","There","!",sep="\t") #sep= separator between string - \t equally to tab button pressed
+
+
+print("2")
+print(2)
+print(2.0)
+print(0.2)
+print(0o123) #0o asas lapan/octal format
+print(0xA) #0x bentuk hexadecimal
+print(0.0005)
+print(0.00005)
+print(0.000005)
+
+
+print("Tambah =",5+5)
+print("Tolak =",5-5)
+print("Darab =",5*5)
+print("Bahagi =",5/5)
+print("5 kuasa 5 =",5**5)
+print("Baki hasil bahagi =",5%3)
+
+
+print("Tambah",5+5, sep="=") #guna kaedah separator. = will be in between 
+print("Tolak",5-5, sep="=")
+print("Darab",5*5, sep="=")
+print("Bahagi",10/3, sep="=")
+print("5 kuasa 5",5**5, sep="=")
+print("Baki hasil bahagi",5%3, sep="=")
+
+print(1+5*6**2/2) #proses ikut matematik 
+
+
+
